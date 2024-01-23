fcecfbac1a0a4771247d721fe19f5c5eb6d9524f
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2024-01-24 04:24:29 +0530

    Created a test API route for user

---
+++ api/controller/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+    res.send("User API Testing only")
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
 
 dotenv.config()
 const app = express()
 
 mongoose.connect(process.env.MONGO)
     .then(() => console.log('Connected!'));
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.use('/api/user', userRouter)
 
 app.listen(1000, () => {
     console.log("Server is running on port 1000")
