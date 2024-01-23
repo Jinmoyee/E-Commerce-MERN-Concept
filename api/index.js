40957fa3cb55823ae4036e2eb9fac5a718829635
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2024-01-24 00:06:56 +0530

    Connected o mongdb database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log('Connected!'));
 
 app.listen(1000, () => {
     console.log("Server is running on port 1000")
