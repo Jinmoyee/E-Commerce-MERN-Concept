90d029982d65822525f24f20895c5ca4fc1e6060
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2024-01-25 02:04:42 +0530

    Created a middleware and a function to handle errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js"
 import bcrypt from 'bcryptjs'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     const hashPassword = bcrypt.hashSync(password, 12)
     const newUser = new User({ username, email, password: hashPassword })
     try {
         await newUser.save()
         res.status(201).json("User created successfully")
-    } catch (err) {
-        res.status(500).json(err.message)
+    } catch (error) {
+        next(error)
     }
 }
--- api/index.js
+++ api/index.js
@@ -11,14 +11,19 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO)
     .then(() => console.log('Connected!'));
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
-
 app.use('/api/user', userRouter)
 
 app.use('/api/auth', authRouter)
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
 
 app.listen(1000, () => {
     console.log("Server is running on port 1000")
