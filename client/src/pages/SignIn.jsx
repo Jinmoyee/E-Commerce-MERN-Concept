efaeb81c0c6d1f13c8e4d58f8effedd23f5d2df0
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2024-01-27 07:15:09 +0530

    Added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { signInSuccess, signInStart, signInFailure } from "../redux/user/userSlice";
 
 const SignIn = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const [formData, setFormData] = useState("");
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const { loading, error } = useSelector((state) => state.user)
 
     const handleSignup = async (event) => {
         event.preventDefault();
-        setLoading(true)
+        dispatch(signInStart())
         const res = await fetch('/api/auth/signin', {
             method: 'POST',
             headers: {
@@ -19,11 +21,9 @@ const SignIn = () => {
         });
         const data = await res.json();
         if (data.success == false) {
-            setError(data.message)
-            setLoading(false)
+            dispatch(signInFailure(data.message))
         } else {
-            setLoading(false)
-            setError(null)
+            dispatch(signInSuccess(data))
             navigate('/')
         }
         console.log(data);
