5220339b7ff5ef542ce09a7bcc7a23d09915b41c
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2024-02-05 21:39:35 +0530

    Created update listing API

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorHandeler } from "../utils/error.js"
 
 export const createListing = async (req, res, next) => {
     try {
@@ -24,3 +25,24 @@ export const deleteListing = async (req, res, next) => {
         next(error)
     }
 }
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
